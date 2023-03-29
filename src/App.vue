91bed4aed41d58c6cf79e8ec45ef13244d041926
HaeSeong Jeong <haehoi2014@gmail.com>
2023-03-30 00:22:58 +0900

    first commit and add router

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+      <RouterLink to="/login">로그인</RouterLink> |
+      <RouterLink to="/signup">회원가입</RouterLink>
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
