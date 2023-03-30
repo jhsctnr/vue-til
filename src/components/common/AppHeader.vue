b05dfa8a99f2497084246ade0e6eb0efd2553424
HaeSeong Jeong <haehoi2014@gmail.com>
2023-03-31 00:04:48 +0900

    add logout

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -6,6 +6,7 @@
     <div class="navigations">
       <template v-if="isUserLogin">
         <span class="username">{{ $store.state.username }}</span>
+        <a href="javascript:;" @click="logoutUser">Logout</a>
       </template>
       <template v-else>
         <RouterLink to="/login">로그인</RouterLink>
@@ -20,6 +21,12 @@ export default {
   computed: {
     isUserLogin() {
       return this.$store.getters.isLogin;
+    },
+  },
+  methods: {
+    logoutUser() {
+      this.$store.commit('clearUsername');
+      this.$router.push('/');
     },
   },
 };
